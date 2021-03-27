d731421dcd7767cbfd42f2fafbcc5c13f7deed25
MartinLiptak <liptakmart@gmail.com>
2021-03-27 16:27:04 +0100

    Simple controller

---
+++ CashierAPI/Controllers/PaymentController.cs
@@ -0,0 +1,54 @@
+﻿using CashierAPI.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CashierAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        // GET: api/<PaymentController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PaymentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PaymentController>
+        [HttpPost]
+        public IActionResult Post([FromBody] ProductDTO value)
+        {
+            if (value != null)
+            {
+                return Ok("good");
+            }
+            
+            return BadRequest();
+        }
+
+        // PUT api/<PaymentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PaymentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CashierAPI/DTO/ProductDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashierAPI.DTO
+{
+    public class ProductDTO
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public string Code
+        {
+            get; set;
+        }
+
+        public float UnitPrice
+        {
+            get; set;
+        }
+    }
+}
